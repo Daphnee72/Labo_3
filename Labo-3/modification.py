91fb56da28ded08c97231d9662e2330277dfae63
Daphnee72 <daphneepelletier18@icloud.com>
2025-02-07 16:00:53 -0500

    modification

---
+++ Labo-3/modification.py
@@ -0,0 +1,5 @@
+#modification
+''' au debut javais juste 3 option donc jai rajouter un mutli choice (While) avec 3 choix. si tu ne choisi pas l'option A ou B
+le programme recommence au debut. J'ai egallement mis le break qui fait que lorsque les premiere questions dans le if sont 
+correcte il ne donne pas le choix multipes. J'ai aussi au debut changer mes options et jen ai ragouter une ou deux. 
+'''
