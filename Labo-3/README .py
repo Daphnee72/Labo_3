1ae4518bcd13e46669f3e6f751a767cb17750839
Daphnee72 <daphneepelletier18@icloud.com>
2025-02-07 16:26:26 -0500

    travail et le readme

---
+++ Labo-3/README .py
@@ -0,0 +1,6 @@
+#README .md
+'''le but du programme est de regarder si quelle quun peut me marrier en repondant a des question. en gros on appuis sur le bouton play
+et on repond a la question suivante, si la personne a une reponse de pas correcte au 4 premiere question elle passe au choix de reponse.
+tandique si elle a les 4 correct c'est fini et elle peit me marrier. par la suite si elle est pas kimi ou ollie il doit recommencer
+justqua ce que il reponde loption a ou b. 
+'''
--- Labo-3/travail.py
+++ Labo-3/travail.py
@@ -1,23 +1,23 @@
 #formulaire de mariage
 #information
-F1_Drivers = bool(input("vous etes un pilote de F1 ou F2 ou F1 academy:"))
-buy_me_food = bool(input("payer vous la bouffe en date:"))
-nombres_de_point = int(input("entrer vos points en 2024:"))
-revenu = float(input("quelle est votre revenu par année:"))
-Person = "C"
+F1_Drivers = bool(input("vous etes un pilote de F1 ou F2 ou F1 academy:")) #il faut repondre oui ou non.
+buy_me_food = bool(input("payer vous la bouffe en date:")) #il faut repondre oui ou non
+nombres_de_point = int(input("entrer vos points en 2024:")) #il faut repondre par un nombre plus haut que 200
+revenu = float(input("quelle est votre revenu par année:")) #il faut repondre par un nombre plus haut que 22 millions
+Person = "C" #jai ajoter cette condition la pour le choix multiple plus bas
 
 #reponse
-while Person == "C":
+while Person == "C": #le while pour qui fasse un loop
     if F1_Drivers and buy_me_food  and nombres_de_point > 200 and  revenu > 22000000: #Les conditions a respecter
-        print ("vous pouvez me marier")
+        print ("vous pouvez me marier") #ecrire ce qui en parenthese
         break #si repond deja au condition arreter ici
-    else:
+    else: #si ya un non ou un enonce qui corespond pas afficher le print de la linge d'en bas
         print ("go to the next question")
         
     Person = input("R U: \n A) Kimi A.. B) Ollie B.. C) naur : ") #si non si tes kimi ou Ollie c'est correect
-    if Person == "A":
+    if Person == "A": #si la personne ecrit la lettre A en majuscule
         print("Marry me right now")
-    elif Person == "B":
+    elif Person == "B": #si la personne ecrit la lettre B en majuscule
         print("You can")
-    elif Person == "C": 
+    elif Person == "C": #si la personne ecrit la lettre C en majuscule
         print("Get out")
