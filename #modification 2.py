9337c2d89b6b3c89be0632be453a72f8ec158227
Daphnee72 <daphneepelletier18@icloud.com>
2025-02-14 15:03:04 -0500

    Create #modification 2.py

---
+++ #modification 2.py
@@ -0,0 +1,10 @@
+#modification.py
+'''
+-Jai refaite pour que j'aille plus de ligne de code et en essaiyant de rajouter un systheme de points. 
+-Ensuite j'ai rajouter des boucles for et if. 
+-Par la suite j'ai rajouterd des question ppour repondre au question du travail.
+-Ensuite, J'ai essayer de rajouter des commentaires entre chaque section. 
+-J'ai optimiser mon programme en enlevant quelques truc random.
+-J'ai essayer de rajouter des fonction que j'ai trouver sur google. 
+- J'ai rajouter des strip parce que sur google il disait que sa pourrait etre utile et j'ai voulu l'essayer.
+'''
